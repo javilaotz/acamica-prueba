ff4e2b3bf61bd65eac2cd8088849f459f2c3eeaf
Jhon Avila <javilaortiz@gmail.com>
2020-07-27 21:19:00 -0500

    Creating a JSX file, project not working

--- components/Foto.jsx
+++ components/Foto.jsx
@@ -1,6 +1,7 @@
 export default function Foto() {
     return (
         `<div>
+            <span> Se supone que esto debe funcionar </span>
             <img src="https://dam.ngenespanol.com/wp-content/uploads/2019/05/tortugas-marinas.png" alt="Tortugas marinas"/>
         </div>`
     )
